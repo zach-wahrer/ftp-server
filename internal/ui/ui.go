0f44447d4518cbc2e06e10f90e62eb95289087af
Zach Wahrer <zachtheclimber@gmail.com>
2020-06-24 16:15:25 -0600

    Add test code for TestPrintHelp and func outline

--- internal/ui/ui.go
+++ internal/ui/ui.go
@@ -1,2 +1,8 @@
 // Package ui provides user interface functions for ftp-server
 package ui
+
+// RunCommand runs the input string on the ftp-server
+func RunCommand(cmd string) string {
+
+	return ""
+}
--- internal/ui/ui_test.go
+++ internal/ui/ui_test.go
@@ -15,5 +15,13 @@ func TestOnInitialConnectionOutput(t *testing.T) {
 }
 
 func TestPrintHelp(t *testing.T) {
-
+	reply := RunCommand("help")
+	expected := "Available Commands:\n" +
+		"\t'cd' - change directory\n" +
+		"\t'ls' - list files\n" +
+		"\t'get {filename}' - download file\n" +
+		"\t'put {filename}' - upload local file"
+	if reply != expected {
+		t.Errorf("unexpected func reply: want \"%s\", got \"%s\"", expected, reply)
+	}
 }
